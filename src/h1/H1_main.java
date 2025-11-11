1ee7db0943ecf63cc4045bd45c42d9388cb8243a
oskarcai <oskarcaicai@gmail.com>
2025-11-11 13:57:28 +0100

    1st Entwurf

---
+++ src/h1/H1_main.java
@@ -0,0 +1,26 @@
+package h1;
+
+public class H1_main {
+    public static void main(String[] args) {
+        int zone = 5;
+        double price = 0;
+
+        switch(zone) {
+            case 1:
+                price = 2;
+                break;
+            case 2:
+                price = 2.35;
+                break;
+            case 3:
+            case 4:
+                price = 2.85;
+                break;
+            case 5:
+                price = 3.55;
+                break;
+            default: price = 4;
+        }
+        System.out.println("Ticketpreis bis Zone " + zone + ": " + price + "€");
+    }
+}
---
+++ src/h2/H2_main.java
@@ -0,0 +1,22 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args) {
+        boolean x = true, y = true, a = false, b = false, c = false;
+        int input = 10;
+
+        x = (input == 10) || (input == 11);
+        y = (input == 11) || (input == 1);
+
+        boolean E1 = x;
+        boolean E2 = y;
+        boolean E3 = x != y;
+
+        a = E1 && E2;
+        b = E1 || E3;
+        c = (x && !y) || (!x && !y);
+
+        System.out.println("x: " + x + " y: " + y + " a: " + a + " b: " + b + " c: " + c);
+    }
+}
+
