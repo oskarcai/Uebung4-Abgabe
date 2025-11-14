6fb164d612c190c6bf3499853ddbf2079ca78e3c
oskarcai <oskarcaicai@gmail.com>
2025-11-14 09:59:01 +0100

    float & double

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,11 +2,11 @@ package h3;
 
 public class H3_main {
     public static void main(String[] args) {
-        float celsiusFloat = 14.2F, fahrenheitFloat = 0;
-        double celsiusDouble = 14.2, fahrenheitDouble = 0;
+        float celsiusFloat = 14.2F, fahrenheitFloat = 0F;
+        double celsiusDouble = 14.2, fahrenheitDouble = 0.0;
 
-        fahrenheitFloat = celsiusFloat * 9/5 + 32;
-        fahrenheitDouble = celsiusDouble * 9/5 + 32;
+        fahrenheitFloat = celsiusFloat * 9f/5f + 32f;
+        fahrenheitDouble = celsiusDouble * 9.0/5.0 + 32.0;
 
         System.out.println(celsiusFloat + " Grad Celsius = " + fahrenheitFloat + " Grad Fahrenheit [Float-Datentyp]");
         System.out.println(celsiusDouble + " Grad Celsius = " + fahrenheitDouble + " Grad Fahrenheit [Double-Datentyp]");
